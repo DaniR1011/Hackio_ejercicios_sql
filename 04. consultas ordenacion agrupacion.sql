8579260435b0a227080301e1d3330aea588e31eb
DaniR1011 <recio.dani10@gmail.com>
2024-09-24 17:55:06 +0200

    second upload sql exercises

--- 04. consultas ordenacion agrupacion.sql
+++ 04. consultas ordenacion agrupacion.sql
@@ -10,7 +10,9 @@ ORDER BY "AlbumId" desc
 limit 1;
 
 -- Ejercicio 3:  Calcula la duración promedio, desviación estándar y varianza por compositor.
-
+select "Composer", AVG("Milliseconds"), STDDEV("Milliseconds"), VARIANCE("Milliseconds")
+from "Track"
+group by "Composer";
 
 -- Ejercicio 4: Selecciona el título y el precio de las pistas, y limita los resultados a 10.
 select "Name", "UnitPrice"
@@ -38,21 +40,44 @@ from "Customer"
 group by "Country";
 
 -- Ejercicio 9: Obtén el total de importes de facturas para cada país y muestra solo aquellos con un total superior a 10,000.
-
+select "Total"
+from "Invoice"
+where "Total" > 10.000;
 
 -- Ejercicio 10: Encuentra los países donde el número total de clientes es mayor a 5. Muestra el país y el número de clientes.
-
+select "CustomerId", "Country"
+from "Customer"
+group by "CustomerId" 
+having "CustomerId" > 5; 
 
 -- Ejercicio 11: Obtén la cantidad de pistas en cada tipo de medio (MediaType) que tienen una duración mayor a 250,000 milisegundos.
-   
+select "TrackId", "Milliseconds" 
+from "Track"
+group by "TrackId" 
+having "Milliseconds" > 250000;
 
 -- Ejercicio 12: Lista el país y el número total de clientes que han sido asistidos por un representante de ventas específico, cuyo ID sea menor a 5.
-   
+select "Country", "CustomerId"
+from "Customer"
+where "CustomerId" < 5;
 
 -- Ejercicio 13: Muestra los códigos postales (BillingPostalCode) de los clientes que han hecho más de 5 compras.
-  
+select "PostalCode"
+from "Customer"
+group by "PostalCode"
+having count("CustomerId") > 5;
 
 -- Ejercicio 14: Encuentra la cantidad total de pistas en cada tipo de medio (MediaTypeId) donde el precio de la pista (UnitPrice) es mayor a 0.99 y la duración de la pista (Milliseconds) es mayor a 200,000. Muestra el tipo de medio y el número total de pistas.
-
+select "MediaTypeId", count(*) as Total_Type
+from "Track"
+where "UnitPrice" > 0.99 and "Milliseconds" > 200000
+group by "MediaTypeId";
 
 -- Ejercicio 15: Lista el país y el estado de los clientes que tienen el soporte asignado a un representante con ID mayor a 3 y cuyo nombre de empresa (Company) no es nulo. Muestra el país, el estado y el número de clientes.
+select "Country", "State", count(*) as Numero_clientes
+from "Customer"
+where "SupportRepId" > 3 and "Company" is not null 
+group by "Country", "State";
+
+
+
